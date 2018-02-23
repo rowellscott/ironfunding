5a5034ff81bff62ba5e455b37bd621c1b69fa8ed
Scott Rowell <rowell.scott@gmail.com>
2018-02-23 10:55:51 -0500

    Configure User Signup

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session)
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user');
+const bcrypt = require('bcrypt');
+
 
 mongoose.connect('mongodb://localhost:27017/ironfunds-development')
 var index = require('./routes/index');
+var authRoutes = require('./routes/authentication')
 var users = require('./routes/users');
 
 var app = express();
@@ -36,6 +41,63 @@ app.use(session({
   store: new MongoStore( {mongooseConnection: mongoose.connection})
 }))
 
+passport.serializeUser((user, cb)=>{
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb)=> {
+  User.findById(id, (err, user)=>{
+    if (err) {return cb(err)};
+    cb(null, user)
+  })
+})
+
+passport.use('local-signup', new LocalStrategy(
+  {passReqToCallback: true}, 
+  (req, username, password, next) =>{
+    process.nextTick(()=>{
+      User.findOne({
+        'username':username
+      }, (err, user) => {
+        if (err){return next(err);}
+
+        if (user){
+          return next(null, false)
+        } else {
+          const {username, email, description, password} =req.body
+          const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+          const newUser = new User({
+            username, 
+            email, 
+            description, 
+            password: hashPass
+          });
+
+        newUser.save((err)=>{
+          if (err){next(err);}
+          return next(null, newUser);
+        });
+      };
+    });
+  });
+}));
+
+passport.use('local-login', new LocalStrategy((username, password, next)=>{
+    User.findOne({username}, (err, user)=> {
+        if (err){
+          return next(err);
+        }
+        if (!user){
+          return next(null, false, {message: "Incorrect username"});
+        }
+        if(!bcrypt.compareSync(password, user.password)){
+          return next(null, false, {message: "Incorrect Password"});
+        }
+
+        return next(null, user)
+    })
+}))
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -48,6 +110,7 @@ app.use( (req, res, next)=> {
   next();
 })
 app.use('/', index);
+app.use('/', authRoutes)
 
 
 // catch 404 and forward to error handler
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const passport = require('passport')
 const router = express.Router()
 
 router.get('/signup', (req, res) =>{
   res.render('authentication/signup')
-})
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/',
+  failureRedirect : '/signup'
+}));
 
 module.exports = router
